254c3a64c4500951c03ae0ed0f555ceb4171046d
nicolas-budin <np.budin@gmail.com>
2020-09-08 23:15:16 +0200

    added simple database layer that display the list of users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+// db
+const sqlite3 = require('sqlite3').verbose();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  db = new sqlite3.Database('./sample.db');
+
+  let sql = `SELECT * FROM user ORDER BY id`;
+
+  db.all(sql, [], (err, rows) => {
+
+    if (err) {
+      throw err;
+    }
+
+    rows.forEach((row) => {
+      console.log(row.id);
+    });
+
+    res.render('users', {users: rows});
+
+    // close the database connection
+    db.close();
+
+  });
+
 });
 
 module.exports = router;
