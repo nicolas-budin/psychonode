0e123f0d27dff41336f9e4f7612e28fdcd6b4ca9
nicolas-budin <np.budin@gmail.com>
2020-11-04 17:34:08 +0100

    data definitions in dedicated sql file

---
+++ docs/sql/add_test_data.sql
@@ -0,0 +1,75 @@
+
+delete from test_definition;
+
+
+--
+-- french
+--
+
+-- pre-test set
+
+insert into test_definition (question, answer)
+values ('csirke', 'poulet');
+insert into test_definition (question, answer)
+values ('sajt', 'fromage');
+insert into test_definition (question, answer)
+values ('bors', 'poivre');
+insert into test_definition (question, answer)
+values ('fagyi', 'glace');
+insert into test_definition (question, answer)
+values ('viz', 'eau');
+insert into test_definition (question, answer)
+values ('Farkas', 'Loup');
+insert into test_definition (question, answer)
+values ('Cet', 'Baleine');
+insert into test_definition (question, answer)
+values ('Menni', 'Marcher');
+insert into test_definition (question, answer)
+values ('Rohan', 'Courir');
+insert into test_definition (question, answer)
+values ('Ugat', 'Aboyer');
+insert into test_definition (question, answer)
+values ('Oldal', 'Page');
+insert into test_definition (question, answer)
+values ('Toll', 'Stylo');
+insert into test_definition (question, answer)
+values ('Tanulni', 'Apprendre');
+insert into test_definition (question, answer)
+values ('Szem', 'Oeil');
+insert into test_definition (question, answer)
+values ('Haj', 'Cheveux');
+insert into test_definition (question, answer)
+values ('Orr', 'Nez');
+insert into test_definition (question, answer)
+values ('Penz', 'Argent');
+insert into test_definition (question, answer)
+values ('Nyer', 'Gagner');
+insert into test_definition (question, answer)
+values ('Veszt', 'Perdre');
+insert into test_definition (question, answer)
+values ('Ember', 'Homme');
+insert into test_definition (question, answer)
+values ('Gyerek', 'Enfant');
+insert into test_definition (question, answer)
+values ('Szoba', 'Chambre');
+insert into test_definition (question, answer)
+values ('Asztal', 'Table');
+insert into test_definition (question, answer)
+values ('Konyha', 'Cuisine');
+insert into test_definition (question, answer)
+values ('Aludni', 'Dormir');
+
+
+-- developer set (testing)
+
+insert into test_definition (question, answer, setId)
+values ('csirke', 'poulet', 2);
+insert into test_definition (question, answer, setId)
+values ('sajt', 'fromage', 2);
+insert into test_definition (question, answer, setId)
+values ('bors', 'poivre', 2);
+insert into test_definition (question, answer, setId)
+values ('fagyi', 'glace', 2);
+insert into test_definition (question, answer, setId)
+values ('viz', 'eau', 2);
+
--- docs/sql/init.sql
+++ docs/sql/init.sql
@@ -116,6 +116,12 @@ values (1);
 insert into test_definition_set(id, is_active)
 values (2, true);
 
+insert into test_definition_set(id, is_active, language)
+values (3, false, 'german');
+
+insert into test_definition_set(id, is_active, language)
+values (4, true, 'german');
+
 -- test definition
 create table test_definition
 (
@@ -128,77 +134,6 @@ create table test_definition
     FOREIGN KEY (setId) REFERENCES test_definition_set (id)
 );
 
-
--- pre-test set
-
-insert into test_definition (question, answer)
-values ('csirke', 'poulet');
-insert into test_definition (question, answer)
-values ('sajt', 'fromage');
-insert into test_definition (question, answer)
-values ('bors', 'poivre');
-insert into test_definition (question, answer)
-values ('fagyi', 'glace');
-insert into test_definition (question, answer)
-values ('viz', 'eau');
-insert into test_definition (question, answer)
-values ('Farkas', 'Loup');
-insert into test_definition (question, answer)
-values ('Cet', 'Baleine');
-insert into test_definition (question, answer)
-values ('Menni', 'Marcher');
-insert into test_definition (question, answer)
-values ('Rohan', 'Courir');
-insert into test_definition (question, answer)
-values ('Ugat', 'Aboyer');
-insert into test_definition (question, answer)
-values ('Oldal', 'Page');
-insert into test_definition (question, answer)
-values ('Toll', 'Stylo');
-insert into test_definition (question, answer)
-values ('Tanulni', 'Apprendre');
-insert into test_definition (question, answer)
-values ('Szem', 'Oeil');
-insert into test_definition (question, answer)
-values ('Haj', 'Cheveux');
-insert into test_definition (question, answer)
-values ('Orr', 'Nez');
-insert into test_definition (question, answer)
-values ('Penz', 'Argent');
-insert into test_definition (question, answer)
-values ('Nyer', 'Gagner');
-insert into test_definition (question, answer)
-values ('Veszt', 'Perdre');
-insert into test_definition (question, answer)
-values ('Ember', 'Homme');
-insert into test_definition (question, answer)
-values ('Gyerek', 'Enfant');
-insert into test_definition (question, answer)
-values ('Szoba', 'Chambre');
-insert into test_definition (question, answer)
-values ('Asztal', 'Table');
-insert into test_definition (question, answer)
-values ('Konyha', 'Cuisine');
-insert into test_definition (question, answer)
-values ('Aludni', 'Dormir');
-
-
--- developer set (testing)
-
-insert into test_definition (question, answer, setId)
-values ('csirke', 'poulet', 2);
-insert into test_definition (question, answer, setId)
-values ('sajt', 'fromage', 2);
-insert into test_definition (question, answer, setId)
-values ('bors', 'poivre', 2);
-insert into test_definition (question, answer, setId)
-values ('fagyi', 'glace', 2);
-insert into test_definition (question, answer, setId)
-values ('viz', 'eau', 2);
-
-
-select *
-from test_definition;
 
 --
 -- test
